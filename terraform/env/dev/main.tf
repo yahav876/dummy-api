36e2613eb2b030287d885aecaf071782144d689c
yahavhorev <yahav.horev@vidaa.com>
2025-05-19 16:40:37 +0300

    fix

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -25,23 +25,3 @@ module "ecr" {
   ecr    = var.ecr
 }
 
-
-
-# module "eks" {
-#   source = "../../modules/compute/eks"
-
-#   eks = {
-#     cluster_name    = var.eks.cluster_name
-#     cluster_version = var.eks.cluster_version
-
-#     cluster_endpoint_public_access           = var.eks.cluster_endpoint_public_access
-#     enable_cluster_creator_admin_permissions = var.eks.enable_cluster_creator_admin_permissions
-
-#     cluster_compute_config = var.eks.cluster_compute_config
-
-#     vpc_id     = module.vpc.vpc_id
-#     subnet_ids = module.vpc.private_subnets
-
-#     tags = var.eks.tags
-#   }
-# }
