76680dcd69e0d0d14fecd2d1ff1e3c9fd766afc2
yahavhorev <yahav.horev@vidaa.com>
2025-05-25 13:08:58 +0300

    update dag file + mockoon app

--- dags/dag.py
+++ dags/dag.py
@@ -1,30 +1,42 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-import requests
-import json
 from datetime import datetime
+import requests
 
 def ingest_data():
-    # Call dummy data API
-    resp = requests.get("http://mockingbird.airflow.svc.cluster.local:1511/api/random")
+    resp = requests.get("http://mockoon.airflow.svc.cluster.local:3000/analytics/live-streams/122")
     resp.raise_for_status()
-    data = resp.json()
+    records = resp.json().get('data', [])
 
-    # Insert into Postgres
-    pg_hook = PostgresHook(postgres_conn_id='postgres_default')  # This points to the URI in your secret
+    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
     insert_sql = """
-        INSERT INTO dummy_table (name, email, phone)
-        VALUES (%s, %s, %s)
+        INSERT INTO dummy_stream_sessions (
+          client_name, device_vendor, location_city, location_country,
+          os_name, os_version, referrer_url, session_loaded_at, session_ended_at
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-    pg_hook.run(insert_sql, parameters=(data['name'], data['email'], data['phone']))
+
+    for r in records:
+        pg_hook.run(insert_sql, parameters=(
+            r['client']['name'],
+            r['device']['vendor'],
+            r['location']['city'],
+            r['location']['country'],
+            r['os']['name'],
+            r['os']['version'],
+            r['referrer']['url'],
+            r['session']['loadedAt'],
+            r['session']['endedAt']
+        ))
 
 default_args = {
     'start_date': datetime(2023, 1, 1)
 }
 
-with DAG('dummy_data_pipeline', schedule_interval='@hourly', default_args=default_args, catchup=False) as dag:
-    ingest = PythonOperator(
-        task_id='ingest_dummy_data',
+with DAG('dummy_stream_ingest', schedule_interval='@hourly', default_args=default_args, catchup=False) as dag:
+    PythonOperator(
+        task_id='fetch_and_insert_stream_data',
         python_callable=ingest_data
     )
